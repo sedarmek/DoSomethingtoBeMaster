fc936e147a9cf81989591fb26de359f1a697cb0a
Sergio <sdajinvers@outlook.com>
2022-08-08 20:08:32 -0500

    se modifican funciones

--- src/components/Cuerpo/Cuerpo.js
+++ src/components/Cuerpo/Cuerpo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cuerpo.css';
 
 import Canvas from '../Canvas/Canvas'
@@ -9,8 +9,9 @@ import GeneradorHabilidad from '../GeneradorHabilidad/GeneradorHabilidad';
 function Cuerpo() {
   const defaultNumberHabilities = 3;
   const defaultHour = 50;
-  
+  let localStorageHour = localStorage.getItem('localHours') && 'vacio';
   let [horasHabilidades, setHorasHabilidades] = useState(Array(defaultNumberHabilities).fill(defaultHour))
+
 
   function incrementarHabilidades(){
     setHorasHabilidades([...horasHabilidades, defaultHour])
@@ -19,18 +20,24 @@ function Cuerpo() {
     setHorasHabilidades(horasHabilidades.slice(0, horasHabilidades.length-1))
   }
 
-  function actualizarHorasDeHabilidad(hourChild, indexChild) {
+  function actualizarHoraDeHabilidad(hourChild, indexChild) {
     setHorasHabilidades(()=>{
       horasHabilidades = [...horasHabilidades]
       horasHabilidades.splice(indexChild, 1, parseInt(hourChild))
       return horasHabilidades
     })
   }
-
+  // funcion para guardar el array en el cache
+  function saveInLocalStorage(horas) {
+      localStorageHour = horas
+      // localStorage.setItem('')
+      console.log({localStorageHour})
+  }
+  useEffect(()=>{saveInLocalStorage(horasHabilidades)})
   return (
     <div className="Cuerpo">
       <Canvas HorasHabilidades={horasHabilidades}/>
-      <GeneradorHabilidad className='GeneradorHabilidad' HorasHabilidades={horasHabilidades} IncrementarHabilidades={incrementarHabilidades} DecrementarHabilidades={decrementarHabilidades} ActualizarHorasDeHabilidad={actualizarHorasDeHabilidad}/>
+      <GeneradorHabilidad className='GeneradorHabilidad' HorasHabilidades={horasHabilidades} IncrementarHabilidades={incrementarHabilidades} DecrementarHabilidades={decrementarHabilidades} ActualizarHorasDeHabilidad={actualizarHoraDeHabilidad}/>
       <Cronometro/>
       <Convertidor/>
     </div>
--- src/components/Habilidad/Habilidad.js
+++ src/components/Habilidad/Habilidad.js
@@ -8,12 +8,15 @@ function Habilidad(info) {
     info.GetHourOfChildToParent(hora.target.value, info.keyToChild)
   }
 
+  function getHourFirst(event) {
+    event.target.value = info.HorasHabilidades[info.keyToChild]
+  }
   return(
     <div className="Habilidad">
       <FaPenSquare className='icono_pen'></FaPenSquare>
-      <input type="text" className="input_nombreHabilidad" placeholder='Escribe la habilidad'></input>
-      <input type="number" className="input_horasHabilidad" placeholder='horas' onChange={sendHourToParent}></input>
-      <p>/1000 horas</p>
+      <input type="text" className="input_nombreHabilidad" placeholder='Escribe la habilidad'/>
+      <input type="number" className="input_horasHabilidad" placeholder='horas' onChange={sendHourToParent} max={250}/>
+      <p>{info.HorasHabilidades[info.keyToChild]}/1000 horas</p>
       <FaRegSave className="icono_save"></FaRegSave>
     </div>
   );
