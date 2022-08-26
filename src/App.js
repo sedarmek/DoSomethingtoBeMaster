2a1af87727152b55f67dc13ad3108cace34f9ad0
Sergio <sdajinvers@outlook.com>
2022-08-26 10:55:53 -0500

    se modifica package.json para github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 
-import Cabecera from './components/Cabecera/Cabecera'
-import Cuerpo from './components/Cuerpo/Cuerpo'
+import { Outlet, Link } from "react-router-dom";
+// import Cabecera from './components/Cabecera/Cabecera'
+// import Cuerpo from './components/Cuerpo/Cuerpo'
 // import Pie from './components/Pie/Pie'
 
 function App() {
   return (
     <div className="App">
-      <Cabecera></Cabecera>
-      <Cuerpo></Cuerpo>
-      {/* <Pie></Pie> */}
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/cuerpo">Cuerpo</Link> |{" "}
+        <Link to="/cronometro">Cronometro</Link>|{" "}
+        <Link to="/convertidor">Convertidor</Link>
+      </nav>
+      <Outlet/>
+      {/* <Pie/> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Cuerpo from '../src/components/Cuerpo/Cuerpo';
+import Cronometro from '../src/components/Cronometro/Cronometro';
+import Convertidor from '../src/components/Convertidor/Convertidor';
+
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+      <Route path="cuerpo" element={<Cuerpo />} />
+      <Route path="cronometro" element={<Cronometro />} />
+      <Route path="convertidor" element={<Convertidor />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
