b1b5f3a29817f8c44794a3144e145a99c98a6148
Sergio <sdajinvers@outlook.com>
2022-08-29 10:15:40 -0500

    se cambia la ruta que tiene el link de home por bug

--- src/components/Cabecera/Cabecera.jsx
+++ src/components/Cabecera/Cabecera.jsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 const Cabecera = () => (
   <div className="Cabecera">
     <nav>
-      <NavLink to="" className="link">Home</NavLink>
+      <NavLink to="DoSomethingtoBeMaster" className="link">Home</NavLink>
       <NavLink to="error" className="link">Not found</NavLink>
     </nav>
   </div>
